6f5cd81b357087094fb168a846b2f22846de8447
Muhammad Sammad <muhammadsammad04@gmail.com>
2026-06-24 00:18:37 +0500

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,15 +1,22 @@
 
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
 import pandas as pd
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
+from src.exception import CustomException
+from src.logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
@@ -52,4 +59,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    modeltrainer = ModelTrainer()
+    r2_square = modeltrainer.initiate_model_trainer(train_arr, test_arr, _)
+    print(r2_square)
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -13,5 +14,22 @@ def save_object(file_path, obj):
             os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, 'wb') as f:
             dill.dump(obj, f)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+
+            y_test_pred = model.predict(X_test)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
     except Exception as e:
         raise CustomException(e, sys)
